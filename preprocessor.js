15206dce39ae20cdc3772722714c180c9e5bc131
David Brear <davidbrear@Davids-MacBook-Pro.local>
2016-06-13 20:42:37 -0400

    got testing mocks working

---
+++ preprocessor.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const babel = require('babel-core');
+const jestPreset = require('babel-preset-jest');
+const reactNativePreset = require('babel-preset-react-native');
+
+module.exports = {
+  process(src, filename) {
+    if (babel.util.canCompile(filename)) {
+      return babel.transform(src, {
+        filename,
+        presets: [jestPreset, reactNativePreset],
+        retainLines: true,
+      }).code;
+    }
+    return src;
+  },
+};
