7ba24490bc471f5605d726df4bf347123547254a
David Brear <davidbrear@Davids-MacBook-Pro.local>
2016-06-13 20:45:30 -0400

    got enzyme added

--- src/__tests__/Main.spec.js
+++ src/__tests__/Main.spec.js
@@ -1,6 +1,12 @@
 import Main from '../Main';
+import { shallow } from 'enzyme';
 
 describe('<Main />', () => {
+  it('creates the element', () => {
+    const wrapper = shallow(<Main />);
+
+    expect(wrapper.length).toEqual(1);
+  });
   it('works', () => {
     expect(1 + 4).toEqual(5);
   });
